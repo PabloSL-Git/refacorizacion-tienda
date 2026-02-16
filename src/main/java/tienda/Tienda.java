abe10ebb885454029a69e25ffec1d22476d6bb32
PabloSL-Git <santamarialoredo@gmail.com>
2026-02-16 09:16:39 +0100

    replanteado  objetos

--- src/main/java/tienda/Tienda.java
+++ src/main/java/tienda/Tienda.java
@@ -12,6 +12,11 @@ import java.util.Scanner;
  * - Eliminar números fijos introducidos en el código (no están dentro de variables).
  * - Mejorar la gestión de logs (Patrón Singleton).
  */
+
+/**
+ * Creado objeto producto
+ */
+
 public class Tienda {
 
     public static ArrayList<String> n = new ArrayList<>(); // Nombres
